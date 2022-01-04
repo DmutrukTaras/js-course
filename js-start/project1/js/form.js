57bd7b3b660778ec6d172587902d2ade96b83342
Taras <dmutruk.taras@gmail.com>
2022-01-04 13:58:24 +0200

    project1 XMLHttpRequest with Promise

--- js-start/project1/js/form.js
+++ js-start/project1/js/form.js
@@ -51,30 +51,48 @@ formContacts.addEventListener('submit', function (event) {
     event.preventDefault();
     formContacts.appendChild(statusMessage);
 
-    let request = new XMLHttpRequest();
-    request.open('POST', 'http://localhost/js-course/www/js-start/project1/server.php');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
     let formData = new FormData(formContacts);
     let obj = {};
-
     formData.forEach(function (value, key) {
         obj[key] = value;
     });
-
     let jsonObj = JSON.stringify(obj);
-    request.send(jsonObj);
+    let server = 'http://localhost/js-course/www/js-start/project1/server.php';
+    
+    function postData(data,server) {
+        return new Promise(function (resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.open('POST', server );
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send(data);
 
-    request.addEventListener('readystatechange', function () {
-        if (request.readyState < 4) {
-            statusMessage.innerHTML = `${massage.loading}`;
-        } else if (request.readyState === 4 && request.status === 200) {
-            statusMessage.innerHTML = `${massage.success}`;
-        } else {
-            statusMessage.innerHTML = `${massage.failure}`;
-        }
-    });
-
-    for (let input of inputs) {
-        input.value = '';
+            request.addEventListener('readystatechange', function () {
+                if (request.readyState < 4) {
+                    resolve();
+//                    statusMessage.innerHTML = `${massage.loading}`;
+                } else if (request.readyState === 4 && request.status === 200) {
+                    resolve();
+ //                   statusMessage.innerHTML = `${massage.success}`;
+                } else {
+                    reject();
+//                  statusMessage.innerHTML = `${massage.failure}`;
+                }
+            });
+        });
     }
+
+    postData(jsonObj,server)
+    .then(() =>{statusMessage.innerHTML = `${massage.loading}`;console.log(massage.loading)})
+    .then(() =>statusMessage.innerHTML = `${massage.success}`)
+    .catch(() =>statusMessage.innerHTML = `${massage.failure}`)
+    .finally(clearInput(inputsContacts));
+
+
+    function clearInput(inputs) {
+        for (let input of inputs) {
+            input.value = '';
+        }
+    }
+
 });
