1cc12d2cd63bd8c6cdadb21737d66085befa3501
Taras <dmutruk.taras@gmail.com>
2022-01-06 10:43:58 +0200

    end of course

--- js-start/project1/js/showUser.js
+++
@@ -1,9 +0,0 @@
-// let age = document.getElementById('age');
-// function showUser(surname, name) {
-// alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-// }
-// //apply
-// showUser.apply(age, ["Taras","Dmysryk"]);
-// //bind
-// let show = showUser.bind(age);
-// show("Тарас","Дмитрик");
